7ce52cc8a0d2c39beca617831b833a4928d9258c
gigyen <coolllyod@gmail.com>
2023-03-08 21:34:05 -0500

    push

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = 'mongodb+srv://kwameasante42:Nanakwame40.@cluster0.ssxywc5.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI =
+  "mongodb+srv://kwameasante42:Nanakwame40.@cluster0.ssxywc5.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
 
 app.use(authRoutes);
