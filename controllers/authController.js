33c14959b05ee714dfaf129349a776c2cfc20daa
gigyen <coolllyod@gmail.com>
2023-03-15 22:35:46 -0400

    push

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,25 @@
+// controller actions
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
+//handle errors
 const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: "", password: "" };
 
+  //incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "That email is not registered";
+  }
+
+  //incorrect password
+  if (err.message === "incorrect password") {
+    errors.password = "That password is incorrect";
+  }
+
+  //duplicate errors
   if (err.code === 11000) {
-    errors.email = "that email is already registered";
+    errors.email = "that email is already in use!";
     return errors;
   }
 
@@ -21,7 +34,8 @@ const handleErrors = (err) => {
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, "net ninja secret", {
+  //returns the token
+  return jwt.sign({ id }, "secret message", {
     expiresIn: maxAge,
   });
 };
@@ -36,7 +50,6 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
@@ -51,6 +64,18 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send("user login");
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+};
+
+module.exports.logout_get = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
 };
